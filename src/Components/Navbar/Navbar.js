f237162ffdacba928b66a14f923ec39df52febeb
Rebecca Dias <rebeccadias2510@gmail.com>
2023-12-19 19:35:41 -0800

    Added Navbar

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import logo from "../../Assets/logo.png";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="nav-logo">
+        <img src={logo} alt="logo" />
+      </div>
+
+      <div className="nav-links">
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Projects</a>
+          </li>
+          <li>
+            <a href="/">About</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
