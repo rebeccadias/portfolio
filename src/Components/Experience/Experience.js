17819704f4bea8cdd6580c9c49950ded96428233
Rebecca Dias <rebeccadias2510@gmail.com>
2023-12-21 12:50:40 -0800

    exp css

--- src/Components/Experience/Experience.js
+++ src/Components/Experience/Experience.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./Experience.css";
 
 const Experience = () => {
   return (
-    <div>
+    <div className="exp-container">
       <div className="div-exp1">
         <div className="div-row1">
           <div>IT Student Worker </div>
@@ -13,9 +14,9 @@ const Experience = () => {
         </div>
         <div className="div-row3">
           <div>
-            Structured documentation framework to streamline data acquisition,
+            -Structured documentation framework to streamline data acquisition,
             reporting workflows, and ticketing processes <br />
-            Utilize data analysis techniques within Zendesk Explore to improve
+            -Utilize data analysis techniques within Zendesk Explore to improve
             operational efficiency and advance key performance indicators for
             user satisfaction
           </div>
@@ -31,16 +32,16 @@ const Experience = () => {
         </div>
         <div className="div-row3">
           <div>
-            Implemented and enhanced the customer creation/amend screens to
+            - Implemented and enhanced the customer creation/amend screens to
             in-cooperate the change request as per client requirements resulting
             in minimal user acceptability testing defects raised/fixed/delivered
-            within service level agreements <br /> Performed root cause analysis
-            and supported development activities on defects and project
-            incidents to increase Transactions accuracy <br /> Expedited the
+            within service level agreements <br /> -Performed root cause
+            analysis and supported development activities on defects and project
+            incidents to increase Transactions accuracy <br /> - Expedited the
             agile lifecycle by performing automated unit testing on redesigned
             functional screens for bank tellers including 50+ test cases and
-            improved the efficacy by 70% <br /> Mentored two team-members to aid
-            comprehension of project workflow <br />
+            improved the efficacy by 70% <br /> -Mentored two team-members to
+            aid comprehension of project workflow <br /> <br />
             Awards: Received On The Spot Award for developing good knowledge on
             existing functionality within a short time and deployed critical
             Change Requests in production
@@ -57,9 +58,9 @@ const Experience = () => {
         </div>
         <div className="div-row3">
           <div>
-            Build a cross-platform mobile app vegetable and fruit market
+            -Build a cross-platform mobile app vegetable and fruit market
             business utilizing Flutter and Dart in collaboration with a team of
-            4 <br /> Developed Ul screens from Figma designs with user
+            4 <br /> -Developed Ul screens from Figma designs with user
             authentication, customer support bot and push notifications
           </div>
         </div>
